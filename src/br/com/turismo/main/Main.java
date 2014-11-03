087f1b0a362ef90e130c7463644da2a7b71b3077
dandan103 <danillo.gs103@gmail.com>
2014-11-03 21:27:27 -0200

    Clientes Cadastrados

--- src/br/com/turismo/main/Main.java
+++ src/br/com/turismo/main/Main.java
@@ -4,7 +4,16 @@
  */
 package br.com.turismo.main;
 
+import br.com.turismo.factory.ClienteFactory;
 import br.com.turismo.sistema.AgenciaTurismo;
+import br.com.turismo.factory.HotelFactory;
+import br.com.turismo.factory.QuartoFactory;
+import br.com.turismo.model.Cliente;
+import br.com.turismo.model.Hotel;
+import br.com.turismo.model.Quarto;
+import br.com.turismo.model.TipoQuarto;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -15,7 +24,69 @@ public class Main {
     private static int opcaoSelecionada = -1;
 
     public static void main(String[] args) {
-
+        try {
+            Cliente c = ClienteFactory.getInstance().criar();
+            c.setNome("Bruno Hering Viadao");
+            c.setCpf("242424");
+            
+            c = ClienteFactory.getInstance().criar();
+            c.setNome("Danillo Bonzao");
+            c.setCpf("777777");
+            
+            c = ClienteFactory.getInstance().criar();
+            c.setNome("Bruno Novinho");
+            c.setCpf("123456");
+            
+            Hotel h = HotelFactory.getInstance().criar();
+            h.setNome("Hilton");
+            
+            Quarto q = QuartoFactory.getInstance().criar();
+            q.setIdentificador("22B");
+            q.setTipoQuarto(TipoQuarto.CAMA_KING_SIZE);
+            
+            h.adicionarQuarto(q);
+            
+            q = QuartoFactory.getInstance().criar();
+            q.setIdentificador("30C");
+            q.setTipoQuarto(TipoQuarto.CAMA_SIMPLES);
+            
+            h.adicionarQuarto(q);
+            
+            h = HotelFactory.getInstance().criar();
+            h.setNome("Ritz");
+            
+            q = QuartoFactory.getInstance().criar();
+            q.setIdentificador("2002");
+            q.setTipoQuarto(TipoQuarto.CAMA_QUEEN_SIZE);
+            
+            q = QuartoFactory.getInstance().criar();
+            q.setIdentificador("4003");
+            q.setTipoQuarto(TipoQuarto.DUAS_CAMAS);
+            
+            h.adicionarQuarto(q);
+            
+            h = HotelFactory.getInstance().criar();
+            h.setNome("Copacabana Palace");
+            
+            q = QuartoFactory.getInstance().criar();
+            q.setIdentificador("111");
+            q.setTipoQuarto(TipoQuarto.CAMA_QUEEN_SIZE);
+            
+            h = HotelFactory.getInstance().criar();
+            h.setNome("Mercury");
+            
+            q = QuartoFactory.getInstance().criar();
+            q.setIdentificador("Presidencial I");
+            q.setTipoQuarto(TipoQuarto.PRESIDENCIAL);
+            
+        } catch (InstantiationException ex) {
+            Logger.getLogger(AgenciaTurismo.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(AgenciaTurismo.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NoSuchMethodException ex) {
+            Logger.getLogger(AgenciaTurismo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
         while (opcaoSelecionada != 0) {
             AgenciaTurismo.getInstance().iniciar();
             opcaoSelecionada = Integer.parseInt(AgenciaTurismo.getInstance().lerDados());
--- src/br/com/turismo/sistema/AgenciaTurismo.java
+++ src/br/com/turismo/sistema/AgenciaTurismo.java
@@ -59,55 +59,6 @@ public class AgenciaTurismo {
         acao = new ReservarQuartoAcao();
         acao.setDescricao("Fazer reserva de hotel");
         menuAgenteTurismo.adicionarOpcao(acao);
-        try {
-            Hotel h = HotelFactory.getInstance().criar();
-            h.setNome("Hilton");
-            
-            Quarto q = QuartoFactory.getInstance().criar();
-            q.setIdentificador("22B");
-            q.setTipoQuarto(TipoQuarto.CAMA_KING_SIZE);
-            
-            h.adicionarQuarto(q);
-            
-            q = QuartoFactory.getInstance().criar();
-            q.setIdentificador("30C");
-            q.setTipoQuarto(TipoQuarto.CAMA_SIMPLES);
-            
-            h.adicionarQuarto(q);
-            
-            h = HotelFactory.getInstance().criar();
-            h.setNome("Ritz");
-            
-            q = QuartoFactory.getInstance().criar();
-            q.setIdentificador("2002");
-            q.setTipoQuarto(TipoQuarto.CAMA_QUEEN_SIZE);
-            
-            q = QuartoFactory.getInstance().criar();
-            q.setIdentificador("4003");
-            q.setTipoQuarto(TipoQuarto.DUAS_CAMAS);
-            
-            h.adicionarQuarto(q);
-            
-            h = HotelFactory.getInstance().criar();
-            h.setNome("Copacabana Palace");
-            
-            q = QuartoFactory.getInstance().criar();
-            q.setIdentificador("111");
-            q.setTipoQuarto(TipoQuarto.CAMA_QUEEN_SIZE);
-            
-            h = HotelFactory.getInstance().criar();
-            h.setNome("Mercury");
-            
-            q = QuartoFactory.getInstance().criar();
-            q.setIdentificador("Presidencial I");
-            q.setTipoQuarto(TipoQuarto.PRESIDENCIAL);
-        } catch (InstantiationException ex) {
-            Logger.getLogger(AgenciaTurismo.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            Logger.getLogger(AgenciaTurismo.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (NoSuchMethodException ex) {
-            Logger.getLogger(AgenciaTurismo.class.getName()).log(Level.SEVERE, null, ex);
-        }
     }
 
     public static AgenciaTurismo getInstance() {
