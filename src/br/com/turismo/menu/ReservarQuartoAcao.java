76aa182d86895ab9670a72e166897ae9ccdc641e
BWeninger <bweninger@outlook.com>
2014-11-03 22:08:22 -0200

    Reservar_quarto

--- src/br/com/turismo/menu/ReservarQuartoAcao.java
+++ src/br/com/turismo/menu/ReservarQuartoAcao.java
@@ -7,11 +7,18 @@ package br.com.turismo.menu;
 import br.com.turismo.factory.ClienteFactory;
 import br.com.turismo.factory.HotelFactory;
 import br.com.turismo.factory.PacoteFactory;
+import br.com.turismo.factory.ReservaHotelFactory;
 import br.com.turismo.model.Cliente;
 import br.com.turismo.model.Hotel;
 import br.com.turismo.model.Pacote;
 import br.com.turismo.model.Quarto;
+import br.com.turismo.model.ReservaQuarto;
 import br.com.turismo.sistema.AgenciaTurismo;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 import java.util.logging.Level;
@@ -51,8 +58,19 @@ public class ReservarQuartoAcao extends Acao {
 
             Hotel hotel = selecionarHotel();
             Quarto quarto = selecionarQuarto(hotel);
-            
-            System.out.println("Quantas diárias você deseja?");
+            Integer totalDiarias = obterTotalDiarias();
+            Date dataInicio = obterDataInicio();
+            System.out.println("O total da estadia é R$ " + (quarto.getTipoQuarto().getPreco() * totalDiarias));
+            if (confirmarReserva()) {
+                ReservaQuarto reserva = ReservaHotelFactory.getInstance().criar();
+                reserva.setDataInicio(dataInicio);
+                Calendar cal = Calendar.getInstance();
+                cal.setTime(dataInicio);
+                cal.add(Calendar.DAY_OF_MONTH, totalDiarias);
+                reserva.setDataFim(cal.getTime());
+                pkg.adicionarReservaQuarto(reserva);
+                System.out.println("Reserva criada com sucesso");
+            }
 
         } catch (InstantiationException ex) {
             Logger.getLogger(ReservarQuartoAcao.class.getName()).log(Level.SEVERE, null, ex);
@@ -99,6 +117,43 @@ public class ReservarQuartoAcao extends Acao {
 
             opcao = Integer.parseInt(AgenciaTurismo.getInstance().lerDados());
             result = hotel.getQuartos().get(opcao - 1);
+        }
+        return result;
+    }
+
+    private int obterTotalDiarias() {
+        System.out.println("Insira quantas diárias você deseja?");
+        return Integer.parseInt(AgenciaTurismo.getInstance().lerDados());
+    }
+
+    private Date obterDataInicio() {
+        Date result = null;
+        while (result == null) {
+            System.out.println("Insira a data inicial de sua estadia no seguinte formato (dd/MM/aaaa)");
+            String dataStr = AgenciaTurismo.getInstance().lerDados();
+            DateFormat fmt = new SimpleDateFormat("dd/MM/yyyy");
+            try {
+                result = fmt.parse(dataStr);
+            } catch (ParseException e) {
+                System.out.println("Data invalida.");
+            }
+        }
+
+        return result;
+    }
+
+    private Boolean confirmarReserva() {
+        Boolean result = null;
+        while (result == null) {
+            System.out.println("Confirma a reserva? ");
+            System.out.println("S - SIM");
+            System.out.println("N - NÃO");
+            String opcao = AgenciaTurismo.getInstance().lerDados();
+            if (opcao.equals("S")) {
+                result = true;
+            } else if (opcao.equals("N")) {
+                result = false;
+            }
         }
         return result;
     }
